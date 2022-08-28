b71eeb4428cf8b92f7626c33cbd16ef8f42e4699
Andrej Acevski <aacevski@gmail.com>
2022-08-28 21:21:01 +0200

    feat: Adding authenticated layout

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,17 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+import type { AppProps } from 'next/app'
+import type { ReactElement, ReactNode } from 'react'
 
+import '../styles/globals.css'
 import { AuthProvider } from '~providers/auth-provider'
 
-function App({ Component, pageProps }: AppProps) {
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+
   return (
     <ChakraProvider>
       <AuthProvider>
-        <Component {...pageProps} />
+        {getLayout(
+          <Component {...pageProps} />
+        )}
       </AuthProvider>
     </ChakraProvider>
-
   )
 }
 
